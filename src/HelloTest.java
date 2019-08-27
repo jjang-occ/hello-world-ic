3b4134667335cd26a8e28403e83646e9306b5b6c
justi <justi@JJ-XPS13-LAPTOP.home>
2019-08-27 01:26:17 -0700

    Added a println from the apartment

--- src/HelloTest.java
+++ src/HelloTest.java
@@ -1,11 +1,18 @@
+/**
+ * 
+ * @author JJ
+ * @version 8/27/2019
+ *
+ */
 
 public class HelloTest
 {
-
     public static void main(String[] args)
     {
         // TODO Auto-generated method stub
         System.out.println("wahoo");
+        
+        System.out.println("I imported this at the apartment, added this line, and commit/pushed it back!");
     }
 
 }
